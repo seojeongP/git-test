e59522d7657ae8b6d318430e66f71eced8f7d791
seojeongi <diditjwjd@naver.com>
2020-09-21 00:01:23 +0900

    Create function sum_function[new]

---
+++ calc.py
@@ -0,0 +1,4 @@
+# add program
+def sum_function( n1 , n2):     return n1 + n2
+num1 = int ( input(“ input number 1” ) ) num2 = int ( input(“ input number 2” ) ) sum = num1 + num2
+print(num1 , “+” , num2 ,  “=” , sum )
